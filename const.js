894978e2a73d85918d42088a8b7ecb7342664c20
VASIKARAN R <61900574+VasikaranR@users.noreply.github.com>
2022-03-21 12:12:45 +0530

    Add files via upload

---
+++ const.js
@@ -0,0 +1,14 @@
+const car={
+    carname:"bmw",
+    carmodel:54,
+    carspeed:"450kmp"
+
+
+}
+car.carname="benz";
+console.log(car.carname+ " "+car.carmodel);
+
+/*similar to let but you cannot modify the field 
+// see only if you have the carname cons you cannot change it because the object 
+is only in const not the field simillarly if you try to add new property to the const object 
+named car it will not accept*/
