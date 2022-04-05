9075531cf4eaa568f24d3fe5e8b46171a8d87f44
VASIKARAN R <61900574+VasikaranR@users.noreply.github.com>
2022-04-05 17:07:36 +0530

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,3 @@
+const numbers=[2,4,5,6,9,7,8,12];
+const evenNum=numbers.filter(item=> item%2===0);
+console.log(evenNum);
---
+++ reduce.js
@@ -0,0 +1,3 @@
+const numbers=[1,2,3,4];
+const sum=numbers.reduce((result,item) => result+item);
+console.log(sum);
