4bcc496dafc6d2b654543d08d5c01a4faec2cab0
VASIKARAN R <61900574+VasikaranR@users.noreply.github.com>
2022-04-09 17:27:16 +0530

    Add files via upload

---
+++ angular/modules/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ViewModule } from './view/view.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,ViewModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
