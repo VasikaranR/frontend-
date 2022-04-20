fdef4956fc022491c3c4772d83ecb4d3c50de072
VASIKARAN R <61900574+VasikaranR@users.noreply.github.com>
2022-04-20 10:13:39 +0530

    Update index.js

--- forms/index.js
+++ forms/index.js
@@ -4,16 +4,24 @@ let othersNext=document.getElementById("others-next");
 let workNext=document.getElementById("work-next");
 let educationNext=document.getElementById("education-next");
 let teamNext=document.getElementById("team-next");
+
+let otherPrev=document.getElementById("others-prev");
+let workPrev=document.getElementById("work-prev");
+let educationPrev=document.getElementById("education-prev");
+let teamPrev=document.getElementById("team-prev");
+
 let tab1 = document.getElementById("firstTab")
 let tab2 = document.getElementById("secTab")
 let tab3 = document.getElementById("thirdTab")
 let tab4 = document.getElementById("fourthTab")
 let tab5= document.getElementById("fifthTab")
+let tab6= document.getElementById("sixthTab")
 tab2.style.display="none"
-tab1.style.display="none"
+tab1.style.display="block"
 tab3.style.display="none"
 tab4.style.display="none"
-tab5.style.display="block"
+tab5.style.display="none"
+tab6.style.display="none"
 
 basicNext.addEventListener('click',(event)=>{
     register.getFname();
@@ -26,25 +34,67 @@ basicNext.addEventListener('click',(event)=>{
     register.validateConfirmPassword();
     if(register.checkBasic() == true){
         tab1.style.display="none"
-        tab2.style.display="none"
+        tab2.style.display="block"
     }
 })
 othersNext.addEventListener('click',()=>{
     register.getDate()
     register.validateDate()
-    // register.validateGender()
-    // register.validateState()
     register.getPhone()
     register.validatePhone()
     register.validateGender()
+    register.validateState()
     if(register.checkOthers() == true){
+        console.log(register.checkOthers())
         tab2.style.display="none"
         tab3.style.display="block"
     }
 })
 workNext.addEventListener( 'click',()=>{
-  
+    tab3.style.display="none"
+        tab4.style.display="block"
 })
+educationNext.addEventListener('click',()=>{
+   register.validateGrade()
+   register.getYear()
+   register.validateYear()
+   if(register.checkEducation() == true){
+   
+    tab4.style.display="none"
+    tab5.style.display="block"
+}   
+})
+teamNext.addEventListener('click',()=>{
+    register.validatePract()
+    register.validateAgree()
+    if(register.checkTeam() == true){
+   
+        tab5.style.display="none"
+        tab6.style.display="block"
+    }   
+})
+
+otherPrev.addEventListener('click',()=>{
+    tab1.style.display="block"
+    tab2.style.display="none"
+})
+
+workPrev.addEventListener('click',()=>{
+    tab2.style.display="block"
+    tab3.style.display="none"
+})
+
+educationPrev.addEventListener('click',()=>{
+    tab3.style.display="block"
+    tab4.style.display="none"
+})
+
+teamPrev.addEventListener('click',()=>{
+    tab4.style.display="block"
+    tab5.style.display="none"
+})
+
+
 
 
 class RegForm{
@@ -52,7 +102,8 @@ class RegForm{
      emailreg=/^([a-z]+[\.-\d]*)@$/;
      emailDomainReg=/^([a-z-]+)\.([a-z\-]{2,8})(\.[a-z]{2,8})?$/;
      dateReg=/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(\d{4})$/; 
-     numberReg=/^[6789]{1}[\d]{9}$/;
+     numberReg=/^[6789]{1}[\d]{9}$/;yearErr
+     yearReg = /^(0[1-9]|1[0-2])\/(\d{4})$/
     user={
     fname:"" ,
     fnameErr:"",
@@ -67,7 +118,12 @@ class RegForm{
     phone:"",
     phoneErr:"",
     genderErr:"",
-    stateErr:""
+    stateErr:"",
+    gradeErr:"",
+    year:"",
+    yearErr:"",
+    agreeErr:"",
+    practErr:""
     }
     getFname(){
         this.user.fname=document.getElementById("fname").value;
@@ -87,7 +143,9 @@ class RegForm{
     getPhone(){
         this.user.phone = document.getElementById("phoneNum").value
     }
-    
+    getYear(){
+        this.user.year = document.getElementById("YearsOfPass").value
+    }
     
     validateFname(){
         if(this.user.fname == ""){
@@ -186,14 +244,14 @@ class RegForm{
              else
              {
                  this.removeErrorMsgOthers(0);
-                 this.user.birthDateError = "";
+                 this.user.dateErr = "";
             }
            }
            validateGender(){
             if((document.getElementById("male").checked == false) && (document.getElementById("female").checked == false))
             {
              this.user.genderErr = "Gender required";
-             this.displayErrorMsgOthers(1,this.user.genderErr);
+             this.displayErrorMsgOthers(1,this.user.genderErr);yearErr
             }
             else{
              this.removeErrorMsgOthers(1);
@@ -201,7 +259,7 @@ class RegForm{
             }  
            }
            validatePhone(){
-               console.log("hello")
+              
             if(this.user.phone == "")
             {
                this.user.phoneErr = "Phone number required";
@@ -218,16 +276,67 @@ class RegForm{
                }
            }
            validateState(){
-            let state = document.getElementsByClassName(".state")
-            if(state.options[state.selectedIndex].value == "Choose State")
+            let state = document.getElementById("Vstate")
+            if(state.options[state.selectedIndex].value == "")
             {
              this.user.stateErr = "State required";
-             this.displayOthersErrorMessage(2,this.user.stateErr);
+             this.displayErrorMsgOthers(2,this.user.stateErr);
             }
             else{
-             this.removeOthersErrorMessage(2);
+             this.removeErrorMsgOthers(2);
              this.user.stateErr = "";
             }
+           }
+           validateGrade(){
+            let graduation = document.getElementById("graduation")
+            if(graduation.options[graduation.selectedIndex].value == "")
+            {
+             this.user.gradeErr = "Graduation required";
+             this.displayErrorMsgEducation(0,this.user.gradeErr);
+            }
+            else{
+             this.removeErrorMsgEducation(0);
+             this.user.gradeErr = "";
+            }
+           }
+           validateYear(){
+               if(this.user.year == "")
+               {
+                this.user.yearErr = "year of pass required";
+                this.displayErrorMsgEducation(1,this.user.yearErr);
+               }
+            else if(this.yearReg.test(this.user.year) == false){
+                this.user.yearErr = "Invalid format (mm/YYYY)";
+             this.displayErrorMsgEducation(1,this.user.yearErr);
+            }
+            else{
+                this.removeErrorMsgEducation(1);
+                this.user.yearErr = "";
+            }
+           }
+           validatePract(){
+            let pract = document.getElementById("practice")
+            if(pract.options[pract.selectedIndex].value == "")
+            {
+             this.user.practErr = "Practice required";
+             this.displayErrorMsgTeam(0,this.user.practErr);
+            }
+            else{
+             this.removeErrorMsgTeam(0);
+             this.user.practErr = "";
+            }
+           }
+           validateAgree(){
+               let agree = document.getElementById("agree")
+               if(agree.checked == false)
+               {
+                this.user.agreeErr = "Tick required";
+                this.displayErrorMsgTeam(1,this.user.agreeErr);
+               }
+               else{
+                this.removeErrorMsgTeam(1);
+                this.user.agreeErr = "";
+               }
            }
     checkBasic(){
         let result = false
@@ -241,8 +350,25 @@ class RegForm{
         if(this.user.dateErr=="" && this.user.genderErr=="" && this.user.phoneErr=="" && this.user.stateErr=="" ){
              result = true
         }
+        console.log(this.user.dateErr,this.user.genderErr,this.user.phoneErr,this.user.stateErr)
+        console.log(result)
         return result;
     }
+    checkEducation(){
+        let result = false
+        if(this.user.gradeErr=="" && this.user.yearErr==""){
+             result = true
+        }
+        return result;
+    }
+    checkTeam(){
+        let result = false
+        if(this.user.practErr=="" && this.user.agreeErr==""){
+             result = true
+        }
+        return result;
+    }
+    
     
     displayErrorMsgBasic(index,message){
         const form_group = document.getElementsByClassName("form-group-basic")[index];
@@ -266,7 +392,28 @@ class RegForm{
         form_group.classList.add("valid");
         form_group.getElementsByTagName("span")[0].textContent = "";
     }
-
+    displayErrorMsgEducation(index,message){
+        const form_group = document.getElementsByClassName("form-group-education")[index];
+        form_group.classList.add("invalid");
+        form_group.getElementsByTagName("span")[0].textContent = message;
+    }
+    removeErrorMsgEducation(index){
+        const form_group = document.getElementsByClassName("form-group-education")[index];
+        form_group.classList.remove("invalid");
+        form_group.classList.add("valid");
+        form_group.getElementsByTagName("span")[0].textContent = "";
+    }
+    displayErrorMsgTeam(index,message){
+        const form_group = document.getElementsByClassName("form-group-team")[index];
+        form_group.classList.add("invalid");
+        form_group.getElementsByTagName("span")[0].textContent = message;
+    }
+    removeErrorMsgTeam(index){
+        const form_group = document.getElementsByClassName("form-group-team")[index];
+        form_group.classList.remove("invalid");
+        form_group.classList.add("valid");
+        form_group.getElementsByTagName("span")[0].textContent = "";
+    }
 
 }
 
