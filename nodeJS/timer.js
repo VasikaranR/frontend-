dd18e713f6b81a0e0c9430904d457ba7deb5c6ae
VASIKARAN R <61900574+VasikaranR@users.noreply.github.com>
2022-05-26 20:03:36 +0530

    Add files via upload

---
+++ nodeJS/timer.js
@@ -0,0 +1,18 @@
+setImmediate(() => console.log("setImmediate is called"))
+setTimeout(() => console.log("setTimeout executed"),1000)
+var count = 0
+let result = setInterval(()=>{
+    if(count < 5)
+      console.log("Hello")
+    else
+      clearInterval(result)
+    count++
+    },1000)
+var promise=new Promise((res,rej)=>{
+    res();
+
+}).then(()=>console.log("resolve"))
+process.nextTick(()=>{
+    console.log("Processing nextTick ")
+})
+console.log("Start")
