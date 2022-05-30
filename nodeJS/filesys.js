7c890b71ec3f8e508ce54de255d55924afd0968f
VASIKARAN R <61900574+VasikaranR@users.noreply.github.com>
2022-05-30 08:26:16 +0530

    Add files via upload

---
+++ nodeJS/filesys.js
@@ -0,0 +1,13 @@
+const fs=require('fs');
+
+
+const data =fs.unlinkSync('newname.txt');
+
+// const data =fs.writeFileSync('pro.js','newpro.js');
+
+// const data =fs.copyFileSync('pro.js','copy.js');
+
+// const data =fs.writeFileSync('index.js','hello vasikaran');
+
+// const data=fs.renameSync('index.js','renamed.js');
+
