743d70c5166487516c4734e51e1bc3bcabe6e1ac
VASIKARAN R <61900574+VasikaranR@users.noreply.github.com>
2022-05-30 19:45:36 +0530

    Add files via upload

---
+++ nodeJS/clusters.js
@@ -0,0 +1,18 @@
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died and new one created`);
+    cluster.fork();
+  });
+}
+console.log(`Worker ${process.pid} started`);
